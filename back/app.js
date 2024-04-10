51b9b4e7548e392c4f419d8d9fef4c4379461b33
Mireya Isabel Rodríguez Pasillas <133801089+mirp01@users.noreply.github.com>
2024-04-10 09:59:25 -0600

    clase3

--- back/app.js
+++ back/app.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
 
 app.post('/api/test', (req, res) => {
     res.json({
         greeting: "Hello World!"
     });
+});
+
+app.post('/api/customG', (req, res) => {
+    const name = req.body.name;
+    res.json({
+        greeting: `Hello ${name}!`
+    })
 });
 
 app.listen(port, () => {
--- front/js/script.js
+++ front/js/script.js
@@ -19,3 +19,22 @@ document.getElementById('increment').addEventListener('click', function() {
 function incrementCounter() {
     counterValue++;
 }
+
+// Form function to send the name to the API server
+
+document.getElementById('nameForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+
+    fetch('http://localhost:3000/api/customG', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({name: name}) //llave: variable
+        })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('serverResponse').innerHTML = data.greeting;
+        })
+});
